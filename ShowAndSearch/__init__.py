606a4b66693d583b38e8e0fedf1f484b02b37f05
guchengxi1994 <528490652@qq.com>
2020-09-15 14:02:33 +0800

    1

---
+++ ShowAndSearch/__init__.py
@@ -0,0 +1,11 @@
+'''
+lanhuage: python
+Descripttion: 
+version: beta
+Author: xiaoshuyui
+Date: 2020-09-15 13:53:11
+LastEditors: xiaoshuyui
+LastEditTime: 2020-09-15 13:53:56
+'''
+__version__ = '0.0.0'
+__appname__ = 'show and search'
---
+++ testscripts/testlogger.py
@@ -0,0 +1,16 @@
+'''
+lanhuage: python
+Descripttion: 
+version: beta
+Author: xiaoshuyui
+Date: 2020-09-15 13:57:51
+LastEditors: xiaoshuyui
+LastEditTime: 2020-09-15 13:58:49
+'''
+import sys
+sys.path.append('..')
+
+from ShowAndSearch.utils.logger import logger
+
+if __name__ == "__main__":
+    logger.info("test infomation")
